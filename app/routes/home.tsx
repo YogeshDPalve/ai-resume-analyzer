52fe4db18c5b99458830f70d2f973a671ea8eb6e
Yogesh Palve <dnyyogesh@gmail.com>
2025-08-15 10:26:16 +0530

    Authentication

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,6 +2,9 @@ import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
 import { resumes } from "../../constants/index";
 import ResumeCard from "~/components/ResumeCard";
+import { usePuterStore } from "~/lib/puter";
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
 ("../components/resumeCart");
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,8 +12,16 @@ export function meta({}: Route.MetaArgs) {
     { name: "description", content: "Smart feedback for your dream job" },
   ];
 }
-
+ 
 export default function Home() {
+  const { isLoading, auth } = usePuterStore();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!auth.isAuthenticated) {
+      navigate("/auth?next=/");
+    }
+  }, [auth.isAuthenticated]);
   return (
     <main className="bg-[url('/images/bg-main.svg')] bg-cover">
       <Navbar />
